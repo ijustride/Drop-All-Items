a2d25e83764acf51a652df34b62c5cf2906308b0
ijustride <artekovec@outlook.com>
2024-01-30 18:29:58 +0700

    Creating a mod to drop all held items by a player in Lethal Company

---
+++ DropAllItems/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,27 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Windows;
+
+namespace DropAllItems.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void dropAllItemsPatch(PlayerControllerB __instance)
+        {
+            if (BepInEx.UnityInput.Current.GetKeyDown("T"))
+            {
+                DropAllItemsModBase.mls.LogInfo("Trying to discard of all objects");
+                __instance.DropAllHeldItems();
+            }
+        }
+    }
+}
---
+++ DropAllItems/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using DropAllItems.Patches;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropAllItems
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class DropAllItemsModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "brooklynbaby.DropAllItems";
+        private const string modName = "DropAllItems";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static DropAllItemsModBase Instance;
+
+        public static ManualLogSource mls;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("The DropAllItems mod has awaken :");
+            harmony.PatchAll(typeof(DropAllItemsModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
